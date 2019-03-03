78fd1f4cc6c7fbab6951cf8efcedfc2994617fd5
mehremic1 <mehremic.a@gmail.com>
2019-03-03 11:49:29 +0100

    add files

---
+++ cura_bot.py
@@ -0,0 +1,21 @@
+
+
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+import requests
+
+# created event Handler. Constantly polling for new messages
+updater = Updater(token="634521747:AAFdi1i8pMIKECEdYBrOvIRdGJNCAtlpWSU")
+# add new event handlers
+dispatcher = updater.dispatcher
+
+def cura(bot, update):
+    req = requests.get("https://github.com/mehremic1/telegram-bot/blob/master/response.json")
+    response = req.json()[0]["title"]+"\n" + req.json()[0]["body"]
+    update.message.reply_text(response)
+
+dispatcher.add_handler(MessageHandler(Filters.text, cura))
+
+# start polling
+updater.start_polling()
+
+updater.idle()
